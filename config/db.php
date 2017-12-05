5a56ed402b947487559c1b320d02ac7d913ee448
BorisGBB <32562779+BorisGBB@users.noreply.github.com>
2017-12-05 17:01:17 +0300

    Add files via upload

---
+++ config/db.php
@@ -0,0 +1,5 @@
+<?php
+    define('HOST', 'localhost');
+    define('USER', 'root');
+    define('PASSWORD', '25522552');
+    define('DATABASE', 'forum');
---
+++ config/fields.php
@@ -0,0 +1,9 @@
+<?php
+    define('REG_FORM', array(
+        'name'      =>  array('name' => 'Имя',              'filter' => FILTER_DEFAULT,         'type' => 'text'),
+        'surname'   =>  array('name' => 'Фамилия',          'filter' => FILTER_DEFAULT,         'type' => 'text'),
+        'phone'     =>  array('name' => 'Номер телефона',   'filter' => FILTER_DEFAULT,         'type' => 'tel'),
+        'email'     =>  array('name' => 'Email',            'filter' => FILTER_VALIDATE_EMAIL,  'type' => 'email'),
+        'login'     =>  array('name' => 'Логин',            'filter' => FILTER_DEFAULT,         'type' => 'text'),
+        'password'  =>  array('name' => 'Пароль',           'filter' => FILTER_DEFAULT,         'type' => 'password')
+    ));
